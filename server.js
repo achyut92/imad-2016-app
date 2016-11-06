542dbc75a96aa2bd66d7acff4c27ef028950ff06
achyut <a0134598@u.nus.edu>
2016-11-06 10:13:19 +0800

    create-user feature

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 var Pool = require('pg').Pool;
 var config = {
     user: 'achyut92',
@@ -12,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.username(bodyParser.json());
 
 var pool = new Pool(config); 
 
@@ -61,6 +65,31 @@ app.get('/submit-name', function(req,res){
 	var name = req.query.name;
 	names.push(name);
 	res.send(JSON.stringify(names));
+});
+
+function hash(input,salt){
+	var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+	return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input', function(req,res){
+	var hashedString = hash(req.params.input, 'this-is-some-string');
+	res.send(hashedString);
+});
+
+app.post('/create-user', function(req,res){
+
+	var username = req.body.username;
+	var password = req.body.password;
+	var salt = crypto.getRandomBytes(128).toString('hex');
+	var dbString = hash(password,salt);
+	pool.query('INSERT INTO "user" (username,password) VALUES ($1, $2',[username,dbString], function(err,result){
+		if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result.rows));
+       }
+   });
 });
 
 app.get('/test-db',function(err,res){
