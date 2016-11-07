31ba7ade1ffc7adbebf9d97cdf94745f06542f6b
achyut <a0134598@u.nus.edu>
2016-11-07 13:28:59 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.get('/check-login', function(req,res){
 
 app.get('/logout', function(req,res){
 	delete req.session.auth;
-	res.send('You are logged out.');
+	res.redirect('/');
 });
 
 app.get('/test-db',function(err,res){
