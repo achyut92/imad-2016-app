e4edfddaaa596285c4de0788d4e5ca3c4778a2d5
achyut <a0134598@u.nus.edu>
2016-11-07 13:19:37 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.post('/login', function(req,res){
 
 app.get('/check-login', function(req,res){
 	if(req.session && req.session.auth && req.session.auth.userId){
-    	pool.query('SELECT * FROM "user" WHERE username=$1',[username], function(err,result){
+    	pool.query('SELECT * FROM "user" WHERE id=$1',[req.session.auth.userId], function(err,result){
     		if(err){
                res.status(500).send(err.toString());
            }else{
