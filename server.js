1af6344daaf567ea96455834e618b73ec02cdd30
achyut <a0134598@u.nus.edu>
2016-11-07 11:21:41 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.use(session({
 	cookie: {maxAge: 1000*60*24*30}
 }));
 
+app.set('views',path.join(__dirname,'ui'));
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
 
 
 var pool = new Pool(config); 
