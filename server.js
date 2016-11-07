d9d2aad6de52564f63facda2718889a21deabd6f
achyut <a0134598@u.nus.edu>
2016-11-07 11:22:39 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.get('/test-db',function(err,res){
 
 app.get('/', function (req, res) {
     if(req.session && req.session.auth && req.session.auth.userId){
-		res.render(path.join(__dirname, 'ui', 'index.html'),{user:req.session.auth.userId.toString()});
+		res.render(path.join('index.html'),{user:req.session.auth.userId.toString()});
 		//res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 	}else{
 	    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
