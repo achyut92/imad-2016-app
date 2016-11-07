e9eb5ee37c9e8ebe1f84509b2e107b5d98b0fd1d
achyut <a0134598@u.nus.edu>
2016-11-07 13:32:04 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,8 +60,9 @@ function checkLogin(){
             if (request.status === 200) {
                 console.log(this.responseText);
                 loadLoggedInUser(this.responseText);
+                window.location = "/";
             }else{
-                
+                window.location = "/";
             }
         }
     };
