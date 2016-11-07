6955c7334b597731ba43f3f47d76d945e8992b64
achyut <a0134598@u.nus.edu>
2016-11-07 11:42:45 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,11 @@ console.log('Loaded!');
 
 
 var submit = document.getElementById('submit-btn');
-submit.onclick = function(){
+var register = document.getElementById('register-btn');
 
-    var request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
+
+submit.onclick = function(){
 
 	request.onreadystatechange = function () {
 
@@ -26,6 +28,28 @@ submit.onclick = function(){
 	var password = document.getElementById('password').value;
 
 	request.open('POST','http://achyut92.imad.hasura-app.io/login',true);   //achyut92.imad.hasura-app.io
+	request.setRequestHeader('Content-Type','application/json');
+	request.send(JSON.stringify({username:username,password:password}));
+};
+
+register.onclick = function(){
+    request.onreadystatechange = function(){
+        	if(request.readyState === XMLHttpRequest.DONE){
+			if (request.status === 200) {
+				console.log('User registered');
+				alert('Registered successfully');
+			}else if (request.status === 403){
+				alert('Registeration unsuccessfully.');
+			}else if (request.status === 500){
+				alert('Something went wrong with our server.');
+			}
+
+		}
+    };
+    var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+
+	request.open('POST','http://achyut92.imad.hasura-app.io/create-user',true);   //achyut92.imad.hasura-app.io
 	request.setRequestHeader('Content-Type','application/json');
 	request.send(JSON.stringify({username:username,password:password}));
 };
