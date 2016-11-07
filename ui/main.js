e7edfcdcf0fdf4ec3c4d500b33c37b4de14b4c23
achyut <a0134598@u.nus.edu>
2016-11-07 13:13:10 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,10 +58,20 @@ function checkLogin(){
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            }else{
                 
             }
         }
     };
     request.open('GET', '/check-login', true);
     request.send(null);
+}
+
+function loadLoggedInUser(username){
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
 }
