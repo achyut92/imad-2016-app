d04baab88279c33ccc4137e40331e50b5836754e
achyut <a0134598@u.nus.edu>
2016-11-08 19:32:17 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,9 @@ app.post('/create-user', function(req,res){
 
 	var username = req.body.username;
 	var password = req.body.password;
+	if(username === '' || password === ''){
+	    res.status(403).send();
+	}
 	var salt = crypto.randomBytes(128).toString('hex');
 	var dbString = hash(password,salt);
 	pool.query('INSERT INTO "user" (username,password) VALUES ($1, $2)',[username,dbString], function(err,result){
