e4852001b235e0d30a2ba4b71fd66b1ce02a0f8d
achyut <a0134598@u.nus.edu>
2016-11-09 09:46:42 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function createTemplate(data){
 
 	var htmlTemplate = `<html>
 	<head>
-		<title>${title}</title>
+		<title>${heading}</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<link href="/ui/style.css" rel="stylesheet" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -51,6 +51,7 @@ function createTemplate(data){
 		${heading}
 		</h3>
 		<div>${date.toDateString()}</div>
+		<br>
 		<div>
 			${content}
 		</div>
