b0dbdf4d027aa7eafc807c9344064b32520a5924
achyut <a0134598@u.nus.edu>
2016-11-09 09:36:58 +0800

    [imad-console] Updates ui/contact.js

--- ui/contact.js
+++ ui/contact.js
@@ -2,10 +2,17 @@ var feedbackBtn = document.getElementById('feedback-btn');
 
 feedbackBtn.onclick = function() {
 	
+	var name = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var message = document.getElementById('message').value;
+	
 	var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         	if(request.readyState === XMLHttpRequest.DONE){
 			if (request.status === 200) {
+			    name.value='';
+			    email.value='';
+			    message.value='';
 				alert('Thank you for your feedback.');
 			}else if (request.status === 403){
 				alert('Please fill all the details.');
@@ -15,9 +22,7 @@ feedbackBtn.onclick = function() {
 
 		}
     };
-    var name = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	var message = document.getElementById('message').value;
+    
 
 	request.open('POST','http://achyut92.imad.hasura-app.io/submit-feedback',true);   //achyut92.imad.hasura-app.io
 	request.setRequestHeader('Content-Type','application/json');
